724a4e8d102250c4b808a0c04a7903665ecbd6e5
suneraaa <152976607+Sueraaa@users.noreply.github.com>
2024-02-25 17:04:38 +1030

    Adding files

---
+++ EmployeeSQL/Create_Tables.sql
@@ -0,0 +1,51 @@
+--  Create a Departments table
+CREATE TABLE Departments (
+    dept_no VARCHAR (4) NOT NULL ,
+    dept_name VARCHAR (20) NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+
+--  Create a Dept_Employees table
+CREATE TABLE Dept_Employees (
+    emp_no INT (6) NOT NULL,
+    dept_no VARCHAR (4) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+);
+
+--  Create a Dept_Managers table
+CREATE TABLE Dept_Managers (
+    dept_no VARCHAR (4) NOT NULL,
+    emp_no INT (6) NOT NULL,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
+
+--  Create a Employees table
+CREATE TABLE Employees (
+    emp_no INT (6) NOT NULL PRIMARY KEY,
+    emp_title_id VARCHAR (5) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR (20) NOT NULL,
+    last_name VARCHAR (20) NOT NULL,
+    sex VARCHAR (1) NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id)
+);
+
+--  Create a Salaries table
+CREATE TABLE Salaries (
+    emp_no INT (6) NOT NULL PRIMARY KEY,
+    salary INT (6) NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
+
+--  Create a Titles table
+CREATE TABLE Titles (
+    title_id VARCHAR (5) NOT NULL PRIMARY KEY,
+    title VARCHAR (20) NOT NULL
+);
+
+-- I created Departments table first then Titles, Salaries, Employees, Dept_Employees, Dept_Managers respectively. 
---
+++ EmployeeSQL/QuickDBD-export.sql
@@ -0,0 +1,77 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/VLvWO2
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- sketch an ERD of the tables
+
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Titles" (
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" INT(6)   NOT NULL,
+    "emp_title_id" VARCHAR(5)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR(20)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" INT(6)   NOT NULL,
+    "salary" INT(6)   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Dept_Managers" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "emp_no" INT(6)   NOT NULL,
+    CONSTRAINT "pk_Dept_Managers" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "Dept_Employees" (
+    "emp_no" INT(6)   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
+    CONSTRAINT "pk_Dept_Employees" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Managers" ADD CONSTRAINT "fk_Dept_Managers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_Managers" ADD CONSTRAINT "fk_Dept_Managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Employees" ADD CONSTRAINT "fk_Dept_Employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Employees" ADD CONSTRAINT "fk_Dept_Employees_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
